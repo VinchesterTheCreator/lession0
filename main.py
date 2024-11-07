b7e7bb1b2f49b0d88a40f0a76cd1a94637648d23
VinchesterTheCreator <khovanovman@gmail.com>
2024-11-08 03:22:30 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#1st program
+print(9 ** 0.5 * 5)
+#2nd program
+print(99.9 > 99.8 and 1000 != 1000.1)
+#3rd programm
+print(2*2+2)
+print(2*(2+2))
+print(2*(2+2)==(2*2+2))
+#4th program
+print(int(float('123.456') * 10)% 10)
